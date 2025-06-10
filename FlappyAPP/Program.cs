e217e81c4f9d46bd51bc2581e6da9c299fd821d8
Kristoffer <krigjo25@gmail.com>
2025-06-10 12:59:57 +0200

    x

--- FlappyAPP/Program.cs
+++ FlappyAPP/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Runtime.InteropServices;
 
 namespace FlappyAPP;
 
@@ -11,38 +12,75 @@ class Program
         Console.WriteLine("This is a simple console application that simulates a flappy bird game.");
         Console.WriteLine("This application has the obstacle generation feature enabled.");
 
-        DrawGameArea();
+        int row = 12;
+        int column = 73;
+
+        DrawGameArea(row, column, "#");
 
     }
 
-    static void DrawGameArea()
+    static void DrawGameArea(int row, int column, string Obstacle)
     {
-        int blockSize = 21;
-        int inlineSize = 73;
-        char newlineCharacter = '\n';
+
+        int CylinderRow = 6;
+        int CylinderColumn = 5;
+        string newlineCharacter = "(space)";
 
         //  Row
-        for (int i = 0; i < blockSize; i++)
+        for (int i = 0; i < row; i++)
         {
-            if (i == 0 || i == blockSize - 1)
+            if (i == 0 || i == row - 1)
             {
                 //  Column
-                Columns(inlineSize);
+                BlockStartColumns(column, Obstacle);
+            }
+            else if (i == 1)
+            {
+                BlockEndCylinderPipe(CylinderRow, CylinderColumn, Obstacle);
+            }
+            else if (i == row - 2)
+            {
+                BlockStartCylinderPipe(CylinderRow, CylinderColumn, Obstacle);
             }
             else
             {
-                Columns(Obstacle: newlineCharacter);
+                BlockStartColumns(1, newlineCharacter);
             }
         }
     }
 
-    static void Columns(int inlineSize = 1, char Obstacle = '#')
+    static void BlockStartColumns(int column, string Obstacle)
     {
-        for (int j = 0; j < inlineSize; j++)
+        for (int i = 0; i < column; i++)
         {
             Console.Write(Obstacle);
         }
+        Console.Write("\n");
+    }
+
+    static void BlockEndColumns(int column, string Obstacle)
+    {
+        for (int i = column; i > 0; i--)
+        {
+            Console.Write(Obstacle);
+        }
+        Console.Write("\n");
+    }
+
+    static void BlockEndCylinderPipe(int row, int column, string Obstacle)
+    {
+        //  Rows
+        for (int i = 0; i < row; i++)
+        {
+            BlockEndColumns(column, Obstacle);
+        }
+    }
+    static void BlockStartCylinderPipe(int row, int column, string Obstacle)
+    {
+        //  Rows
+        for (int i = 0; i < row; i++)
+        {
+            BlockStartColumns(column, Obstacle);
+        }
     }
 }
- 
-
