43aa075854456e6fdf3390a571b9afde018c59fb
Kristoffer <krigjo25@gmail.com>
2025-06-10 15:05:56 +0200

    x

--- FlappyAPP/Program.cs
+++ FlappyAPP/Program.cs
@@ -9,7 +9,6 @@ class Program
        
    static void Main(string[] args)
     {
-
         Console.WriteLine("Welcome to FlappyAPP!");
         Console.WriteLine("This is a simple console application that simulates a flappy bird game.");
         Console.WriteLine("This application has the obstacle generation feature enabled.");
