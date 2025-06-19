3a67bb6b64b32e7a40547f25b44262384d48f75a
Kristoffer <krigjo25@gmail.com>
2025-06-19 11:16:52 +0200

    Refaktorerte `Sprite` klassen for å forbedre strukturen og la til kommandoer som `Draw()` for å tegne figuren og `VerticalMovement()` for vertikale bevegelserRefactored the Sprite class to improve structure and added methods for drawing and vertical movement.

--- FlappyAPP/Sprite.cs
+++ FlappyAPP/Sprite.cs
@@ -2,5 +2,38 @@ namespace FlappyAPP;
 
 public class Sprite
 {
-    
+    int y;
+    int velocity;
+    bool isElivated;
+
+    public string Draw()
+    {
+        string[] wingUp = {
+            "  \\   /  ",
+            "   \\0/   ",
+        };
+
+        string[] wingDown = {
+            "   /0\\   ",
+            "  /   \\  "
+        };
+
+        return isElivated ? string.Join("\n", wingUp) : string.Join("\n", wingDown);
+    }
+
+    public void VerticalMovement()
+    {
+        if (isElivated)
+        {
+            velocity++;
+            y -= velocity;
+            
+        }
+        else
+        {
+            velocity--;
+            y += velocity;
+        }
+    }
+
 }
