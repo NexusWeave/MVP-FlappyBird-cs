11d98c3e38251570f6e099b007730715b8ede5c6
Kristoffer <krigjo25@gmail.com>
2025-06-19 11:15:23 +0200

    Refaktorete GameLogic sin klasse struktur og la til kommando holdere.

--- FlappyAPP/GameLogic.cs
+++ FlappyAPP/GameLogic.cs
@@ -2,5 +2,27 @@ namespace FlappyAPP;
 
 public class GameLogic
 {
-    
+    Sprite sprite = new Sprite();
+    Background background = new Background();
+
+    public void Start()
+    {
+
+        Background.MakeBackground();
+
+
+    }
+    public static void Update()
+    {
+
+    }
+    public static bool CheckCollision()
+    {
+        bool isCollision = false;
+
+        
+        return isCollision;
+
+    }
+
 }
