f0a165e40d14b425999214dd871ccdb163b68343
Kristoffer <krigjo25@gmail.com>
2025-06-20 10:15:37 +0200

    Refactored Sprite class methods for improved structure; made Draw, Toggle, and IsBirdPixel static, and removed unused instance methods.

--- FlappyAPP/Sprite.cs
+++ FlappyAPP/Sprite.cs
@@ -2,17 +2,45 @@ namespace FlappyAPP;
 
 public class Sprite
 {
-    int y;
-    int velocity;
-    bool isElivated;
+    public int[] pos = new int[2];
+    public int velocity;
+    public bool isElivated;
 
-    public void Draw()
+    static void Draw(int y, int x, char[,] buffer, Sprite sprite)
     {
-        return ;
+
+        for (int i = 0; i < sprite.pos[1]; i++)
+        {
+            for (int j = 0; j < buffer.GetLength(1); j++)
+            {
+                Console.ForegroundColor = IsBirdPixel(buffer, sprite) ? ConsoleColor.Black : ConsoleColor.Green;
+
+                Console.Write(buffer[i, j]);
+            }
+            Console.WriteLine();
+
+        }
     }
 
-    public void VerticalMovement()
+    static public bool Toggle(Sprite sprite)
     {
+        // Toggle the elevation state of the sprite
+        sprite.isElivated = !sprite.isElivated;
 
+        return sprite.isElivated;
+    }
+
+    static void VerticalMovement(Sprite sprite)
+    {
+        sprite.pos[1] += sprite.velocity;
+    }
+
+    // Helper method?
+    static bool IsBirdPixel(char[,] buffer, Sprite sprite)
+    {
+        int x = sprite.pos[0];
+        int y = sprite.pos[1];
+
+        return buffer[y, x] == '/' || buffer[y, x] == '\\' || buffer[y, x] == '0';
     }
 }
