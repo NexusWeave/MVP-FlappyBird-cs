2a4e6b6341c7acd46ba98bff044d2a22fc05daab
Kristoffer <krigjo25@gmail.com>
2025-06-20 11:31:41 +0200

    Refactor Start method to accept parameters for improved functionality

--- FlappyAPP/GameLogic.cs
+++ FlappyAPP/GameLogic.cs
@@ -2,22 +2,43 @@ namespace FlappyAPP;
 
 public class GameLogic
 {
-    Sprite sprite = new Sprite();
+    //Sprite sprite = new Sprite();
     Background background = new Background();
 
-    public void Start()
+
+    static public void Start(int x, int y, char obstacleChar)
     {
+        ScreenText.DrawGameStartScreen(x, y, obstacleChar);
+        
+
     }
-    public static void Update()
+    static public void Update()
     {
 
     }
-    public static void CheckCollision()
-    {
+    
+   /* static public void CollisionCheck(int hitboxStart, int hitboxEnd)
+     {
 
-    }
+         if (birdRow > row - 4) birdRow = row - 4;
+         if (velocity > 0) isWingUp = false;
 
-    public static void DrawGameArea()
-    {}
+         if (pipeX <= hitboxEnd && pipeX + 2 >= hitboxStart &&
+             (birdRow <= pipeGapTop || birdRow + 2 >= pipeGapBottom))
+             GameOver();
+
+         if (birdRow + 1 >= row - 3) GameOver();
+     }
+
+    /* static void ResetGame()
+     {
+         score = 0;
+         birdRow = 6;
+         birdCol = 10;
+         velocity = 0;
+         isWingUp = true;
+         pipeX = 72;
+         Main();
+     }*/
 
 }
