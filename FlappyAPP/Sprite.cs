4749543e47c772a31bf2794533acfe2020ed65c4
Kristoffer <krigjo25@gmail.com>
2025-06-20 13:56:14 +0200

    Refactor Sprite class to improve clarity

--- FlappyAPP/Sprite.cs
+++ FlappyAPP/Sprite.cs
@@ -7,65 +7,6 @@ public class Sprite
     public int[] pos = new int[2];
     public int velocity;
     public bool isElivated;
-    /*
-    public bool IsElivated
-    {
-        get
-        {
-            return isElivated;
-        }
-        set
-        {
-            isElivated = !value;
-        }
-    }
-    public int Velocity
-    {
-        get
-        {
-            return Velocity;
-        }
-        set
-        {
-            Velocity = value > 1 ? 1 : value++;
-
-        }
-    }
-            public Sprite(int x, int y)
-            {
-                pos[0] = x; // x position
-                pos[1] = y; // y position
-                velocity = 1; // default velocity
-                isElivated = false; // default elevation state
-            }
-            void Draw(char[,] buffer)
-            {
-                int x = pos[0];
-                int y = pos[1];
-
-                for (int i = 0; i < y; i++)
-                {
-                    for (int j = 0; j < x; j++)
-                    {
-                        Console.ForegroundColor = IsBirdPixel(buffer) ? ConsoleColor.Black : ConsoleColor.Green;
-
-                        Console.Write(buffer[i, j]);
-                    }
-                    Console.WriteLine();
-
-                }
-            }
-
-            private bool IsBirdPixel(char[,] buffer)
-            {
-                int x = pos[0];
-                int y = pos[1];
-
-                return buffer[y, x] == '/' || buffer[y, x] == '\\' || buffer[y, x] == '0';
-            }
-
-            END OF COMMENT BLOCK
-            */
 
     public static void Draw(char[,] buffer)
     {
