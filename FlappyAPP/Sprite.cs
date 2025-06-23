40f1cbda5467d3ec84b5f5207d5547d144d5bbe6
Kristoffer <krigjo25@gmail.com>
2025-06-23 13:17:57 +0200

    Refactor Sprite class to improve elevation handling and enhance Draw method functionality

--- FlappyAPP/Sprite.cs
+++ FlappyAPP/Sprite.cs
@@ -7,24 +7,65 @@ public class Sprite
     public int[] pos = new int[2];
     public int velocity;
     public bool isElivated;
-
-    public static void Draw(char[,] buffer)
+    /*
+    public bool IsElivated
     {
-        int x = buffer.GetLength(1);
-        int y = buffer.GetLength(0);
-
-        for (int i = 0; i < y; i++)
+        get
         {
-            for (int j = 0; j < x; j++)
-            {
-                Console.ForegroundColor = IsBirdPixel(j, i, buffer) ? ConsoleColor.Black : ConsoleColor.Green;
-
-                Console.Write(buffer[i, j]);
-            }
-            Console.WriteLine();
+            return isElivated;
+        }
+        set
+        {
+            isElivated = !value;
+        }
+    }
+    public int Velocity
+    {
+        get
+        {
+            return Velocity;
+        }
+        set
+        {
+            Velocity = value > 1 ? 1 : value++;
 
         }
     }
+            public Sprite(int x, int y)
+            {
+                pos[0] = x; // x position
+                pos[1] = y; // y position
+                velocity = 1; // default velocity
+                isElivated = false; // default elevation state
+            }
+            void Draw(char[,] buffer)
+            {
+                int x = pos[0];
+                int y = pos[1];
+
+                for (int i = 0; i < y; i++)
+                {
+                    for (int j = 0; j < x; j++)
+                    {
+                        Console.ForegroundColor = IsBirdPixel(buffer) ? ConsoleColor.Black : ConsoleColor.Green;
+
+                        Console.Write(buffer[i, j]);
+                    }
+                    Console.WriteLine();
+
+                }
+            }
+
+            private bool IsBirdPixel(char[,] buffer)
+            {
+                int x = pos[0];
+                int y = pos[1];
+
+                return buffer[y, x] == '/' || buffer[y, x] == '\\' || buffer[y, x] == '0';
+            }
+
+            END OF COMMENT BLOCK
+            */
 
     public static bool Toggle(Sprite sprite)
     {
@@ -37,13 +78,6 @@ public class Sprite
     public static int VerticalMovement(int birdrow, int velocity)
     {
         return birdrow += velocity;
-    }
-
-    // Helper method?
-    public static bool IsBirdPixel(int x, int y, char[,] buffer)
-    {
-
-        return buffer[y, x] == '/' || buffer[y, x] == '\\' || buffer[y, x] == '0';
     }
 
     public static int ResetVelocity(int velocity)
