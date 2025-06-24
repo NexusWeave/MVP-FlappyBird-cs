504cb361e5fe75359e8aec76300a19ad3acc644f
Kristoffer <krigjo25@gmail.com>
2025-06-24 14:23:08 +0200

    Move DrawFrame and IsBirdPixel methods from Utils class to Sprite class for better organization

--- FlappyAPP/Sprite.cs
+++ FlappyAPP/Sprite.cs
@@ -71,7 +71,7 @@ public class Sprite
     {
         // Toggle the elevation state of the sprite
         sprite.isElivated = !sprite.isElivated;
-      
+
         return sprite.isElivated;
     }
 
@@ -83,8 +83,29 @@ public class Sprite
     public static int ResetVelocity(int velocity)
     {
         velocity += 1;
-            if (velocity > 1) velocity = 1;
+        if (velocity > 1) velocity = 1;
         return velocity;
     }
 
+    public static void DrawFrame(char[,] buffer)
+    {
+        int x = buffer.GetLength(1);
+        int y = buffer.GetLength(0);
+
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                Console.ForegroundColor = IsBirdPixel(j, i, buffer) ? ConsoleColor.Black : ConsoleColor.Green;
+
+                Console.Write(buffer[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private static bool IsBirdPixel(int x, int y, char[,] buffer)
+    {
+        return buffer[y, x] == '/' || buffer[y, x] == '\\' || buffer[y, x] == '0';
+    }
 }
