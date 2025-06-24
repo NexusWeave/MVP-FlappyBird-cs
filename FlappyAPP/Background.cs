30d01fe504d82d388f30b86ee59a7310f43b76e5
Kristoffer <krigjo25@gmail.com>
2025-06-24 13:30:03 +0200

    Remove SetGameBorders method from Background class to streamline code

--- FlappyAPP/Background.cs
+++ FlappyAPP/Background.cs
@@ -6,17 +6,5 @@ public class Background
     {
         ConsoleSettings.ConsoleConfig();
     }
-    public static char[,] SetGameBorders(char[,] buffer, char obstacleChar)
-    {
-        int x = buffer.GetLength(1);
-        int y = buffer.GetLength(0);
-
-        for (int i = 0; i < x; i++)
-        {
-
-            buffer[y - 1, i] = obstacleChar;
-            
-        }
-        return buffer;
-    }
 }
+
