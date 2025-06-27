8c1ba5b06e044aaba2b19a72d3cfd9f7db55c41d
Kristoffer <krigjo25@gmail.com>
2025-06-27 11:07:53 +0200

    Rename CreateBackground method to GenerateBackground for consistency

--- FlappyAPP/Background.cs
+++ FlappyAPP/Background.cs
@@ -2,7 +2,7 @@ namespace FlappyAPP;
 
 public class Background
 {
-    public static void CreateBackground(char[,] buffer, char obstacle, ref int offset, ref int intervalX)
+    public static void GenerateBackground(char[,] buffer, char obstacle, ref int offset, ref int intervalX)
     {
         Ground.GroundAnimation(ref offset, ref intervalX);
         Ground.DrawGround(ref buffer, obstacle, offset, intervalX);
