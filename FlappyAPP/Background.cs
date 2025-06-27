a570a1199154c20c00c3b649edbe3de9d653eb79
Kristoffer <krigjo25@gmail.com>
2025-06-27 11:04:42 +0200

    Remove commented-out DrawFrame method call in MakeBackground for cleaner code

--- FlappyAPP/Background.cs
+++ FlappyAPP/Background.cs
@@ -8,6 +8,7 @@ public class Background
 
         Ground.GroundAnimation(ref offset, ref intervalX);
         Ground.DrawGround(ref buffer, obstacle, offset, intervalX);
+        //Bird.DrawFrame(ref buffer);
     }
 
 }
