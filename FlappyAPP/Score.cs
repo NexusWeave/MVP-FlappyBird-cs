1ec175c06d1ea323823ccb411a459e78f0c48476
krigjo25 <krigjo25@gmail.com>
2025-07-14 17:05:13 +0200

    Refactor ScoreLogic method: update parameter names for consistency and improve clarity

--- FlappyAPP/Score.cs
+++ FlappyAPP/Score.cs
@@ -8,15 +8,16 @@ namespace FlappyAPP
 {
     internal class Score
     {
-      public static void ScoreLogic(ref int pipeX, ref int bufferWidth, ref int birdCol, ref int birdRow, ref int pipeGapTop, ref int pipeGapBottom, ref int score)
+      public static void ScoreLogic(ref int pipeX, int bufferX, int birdCol, int birdRow, int pipeGapTop, int pipeGapBottom, ref int score)
         {
             pipeX--;
             if (pipeX < -3)
-                pipeX = bufferWidth;
+                pipeX = bufferX;
 
-            int birdHitboxStart = birdCol + 1;
             int birdHitboxEnd = birdCol + 1;
-            if ((pipeX <= birdHitboxEnd && pipeX + 1 >= birdHitboxStart) &&
+            int birdHitboxStart = birdCol + 1;
+            
+            if (pipeX <= birdHitboxEnd && pipeX + 1 >= birdHitboxStart &&
                 (birdRow <= pipeGapTop || birdRow + 1 >= pipeGapBottom))
                 Program.GameOver();
 
