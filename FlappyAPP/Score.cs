f5584cfd2a494a5458c93c966960680c0710a0b0
krigjo25 <krigjo25@gmail.com>
2025-07-14 18:02:43 +0200

    Refactor ScoreLogic method: improve code formatting for clarity

--- FlappyAPP/Score.cs
+++ FlappyAPP/Score.cs
@@ -8,7 +8,7 @@ namespace FlappyAPP
 {
     internal class Score
     {
-      public static void ScoreLogic(ref int pipeX, int bufferX, int birdCol, int birdRow, int pipeGapTop, int pipeGapBottom, ref int score)
+        public static void ScoreLogic(ref int pipeX, int bufferX, int birdCol, int birdRow, int pipeGapTop, int pipeGapBottom, ref int score)
         {
             pipeX--;
             if (pipeX < -3)
@@ -16,7 +16,7 @@ namespace FlappyAPP
 
             int birdHitboxEnd = birdCol + 1;
             int birdHitboxStart = birdCol + 1;
-            
+
             if (pipeX <= birdHitboxEnd && pipeX + 1 >= birdHitboxStart &&
                 (birdRow <= pipeGapTop || birdRow + 1 >= pipeGapBottom))
                 Program.GameOver();
@@ -25,6 +25,12 @@ namespace FlappyAPP
             {
                 score += 1;
             }
+        }
+        public static void DrawScore(int score)
+        {
+            Console.SetCursorPosition(37, 0);
+            Console.WriteLine("Score: " + score, Console.ForegroundColor = ConsoleColor.Black);
+            Console.SetCursorPosition(0, 1);
         }
     }
 }
