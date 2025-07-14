0b06f7767776bdcbed99c70af93cc9dcc370938b
krigjo25 <krigjo25@gmail.com>
2025-07-14 16:09:17 +0200

    Refactor GameLogic methods: streamline buffer initialization and improve parameter handling for DrawStartScreen and DrawGameArea

--- FlappyAPP/GameLogic.cs
+++ FlappyAPP/GameLogic.cs
@@ -9,65 +9,59 @@ namespace FlappyAPP
 {
     internal class GameLogic
     {
-        public static void DrawStartScreen(ref int row, ref int column)
+        private static char[,] InitializeBuffer(int y, int x)
         {
-            int bufferHeight = row;
-            int bufferWidth = column;
-            char[,] buffer = new char[bufferHeight, bufferWidth];
+            return new char[y, x];
+        }
+        public static void DrawStartScreen(ref int bufferY, ref int bufferX)
+        {
+            char[,] buffer = InitializeBuffer(bufferY, bufferX);
 
             ConsoleConfig.ConsoleConfigs();
-            Console.CursorVisible = false;
 
             while (true)
             {
-                for (int y = 0; y < bufferHeight; y++)
-                    for (int x = 0; x < bufferWidth; x++)
+                for (int y = 0; y < bufferY; y++)
+                    for (int x = 0; x < bufferX; x++)
                         buffer[y, x] = ' ';
 
                 ScreenText.MainScreenText(buffer);
+
                 Sprite.LetsFlap(buffer);
 
-               
                 Console.SetCursorPosition(0, 0);
-                DrawScreenFromBuffer(ref bufferHeight, ref bufferWidth, ref buffer);
+                DrawScreenFromBuffer(buffer, bufferY, bufferX);
 
-                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar)
-                    break;
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar) break;
 
                 Thread.Sleep(100);
             }
         }
 
 
-        public static void DrawGameArea(ref int row,ref int column,ref char obstacleChar,ref int velocity,ref int birdRow,ref bool isWingUp,ref int pipeX,ref int birdCol, ref int pipeGapTop, ref int pipeGapBottom,ref int score, ref int offset, ref int intervalWidth)
+        public static void DrawGameArea(int bufferY, int bufferX, char obstacleChar,ref int velocity,ref int birdRow,ref bool isWingUp,ref int pipeX,ref int birdCol, ref int pipeGapTop, ref int pipeGapBottom,ref int score, ref int offset, ref int intervalWidth)
         {
-            int bufferHeight = row;
-            int bufferWidth = column;
-            char[,] buffer = new char[bufferHeight, bufferWidth];
+            char[,] buffer = InitializeBuffer(bufferY, bufferX);
 
             while (true)
             {
-
                 if (Console.KeyAvailable)
                 {
                     if (Console.ReadKey(true).Key == ConsoleKey.Spacebar)
                     {
                         velocity = -2;
-
-                        if (isWingUp) isWingUp = false;
-                        else isWingUp = true;
+                        if (isWingUp) isWingUp = false; else isWingUp = true;
                     }
                     while (Console.KeyAvailable) Console.ReadKey(true);
                 }
 
-                BirdSpeed(ref velocity,ref birdRow,ref row,ref isWingUp);
+                BirdSpeed(ref velocity,ref birdRow);
 
-                Score.ScoreLogic(ref pipeX,ref bufferWidth,ref birdCol,ref birdRow,ref pipeGapTop,ref pipeGapBottom,ref score);
+                Score.ScoreLogic(ref pipeX,ref bufferX,ref birdCol,ref birdRow,ref pipeGapTop,ref pipeGapBottom,ref score);
 
+                if (birdRow >= bufferY -1 ) Program.GameOver();
 
-                if (birdRow >= row -1 ) Program.GameOver();
-
-                Background.SetBuffer(ref bufferHeight, ref bufferWidth, ref buffer, ref obstacleChar, ref offset, ref intervalWidth);
+                Background.SetBuffer( ref buffer,ref offset, ref intervalWidth);
 
                 Obstacle.DrawPipeInBuffer(buffer, pipeX, pipeGapTop, pipeGapBottom, obstacleChar);
                 Sprite.DrawBirdInBuffer(buffer, birdCol, birdRow, velocity);
@@ -76,17 +70,17 @@ namespace FlappyAPP
                 Console.WriteLine("Score: " + score, Console.ForegroundColor = ConsoleColor.Black);
                 
                 Console.SetCursorPosition(0, 1);
-                DrawScreenFromBuffer(ref bufferHeight,ref bufferWidth,ref buffer);
+                DrawScreenFromBuffer(buffer, bufferY, bufferX);
                 
                 Thread.Sleep(50);
             }
         }
 
-        static void DrawScreenFromBuffer(ref int bufferHeight,ref int bufferWidth,ref char[,] buffer) 
+        static void DrawScreenFromBuffer(char[,] buffer, int bufferY,int bufferX) 
         {
-            for (int y = 0; y < bufferHeight; y++)
+            for (int y = 0; y < bufferY; y++)
             {
-                for (int x = 0; x < bufferWidth; x++)
+                for (int x = 0; x < bufferX; x++)
                 {
                     if (Sprite.IsBirdPixel(buffer, y, x))
                         Console.ForegroundColor = ConsoleColor.Black;
@@ -99,7 +93,7 @@ namespace FlappyAPP
             }
         }
 
-        static void BirdSpeed(ref int velocity, ref int birdRow, ref int row,ref  bool isWingUp) 
+        static void BirdSpeed(ref int velocity, ref int birdRow) 
         {
             velocity += 1;
 
